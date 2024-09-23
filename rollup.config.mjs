c542a4b60ee07cb28cc860cc51215968c8d98686
Kwame Opare Asiedu <asieduoparekwame@gmail.com>
2024-09-23 16:20:57 +0000

    Moved rollup plugin into rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -8,9 +8,23 @@ import { dts } from "rollup-plugin-dts";
 import svgr from "@svgr/rollup";
 import paths from "rollup-plugin-tsconfig-paths";
 import shebang from "rollup-plugin-add-shebang";
-import remap from "./plugins/remap-plugin.mjs";
 
-const componentInputMap = globbySync(["src/components/*.tsx", "src/components/**/index.tsx"]).reduce(
+function externals(mappings = {}) {
+  return {
+    name: "externals",
+    renderChunk(code, chunk, options) {
+      let transformedCode = code;
+
+      for (const [from, to] of Object.entries(mappings)) {
+        transformedCode = transformedCode.replace(from, to);
+      }
+
+      return transformedCode;
+    },
+  };
+}
+
+const componentPathMap = globbySync(["src/components/*.tsx", "src/components/**/index.tsx"]).reduce(
   (map, inputPath) => {
     const parts = inputPath.split("/");
     const componentName = inputPath.includes("index") ? parts.at(-2) : parts.at(-1)?.split(".")[0];
@@ -35,7 +49,15 @@ const helperConfigs = defineConfig([
   {
     input: ["src/components/utils.ts"],
     output: { file: "dist/utils.js" },
-    plugins: [nodeResolve(), commonjs(), typescript(), remap(), isProd && terser()],
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      typescript(),
+      externals({
+        "@/components/variants": "./variants.js",
+      }),
+      isProd && terser(),
+    ],
     external: ["react", "react/jsx-runtime", "react-dom", "@/components/variants"],
   },
   {
@@ -55,9 +77,23 @@ const helperConfigs = defineConfig([
 
 const componentConfigs = defineConfig([
   {
-    input: componentInputMap,
+    input: componentPathMap,
     output: { dir: `dist` },
-    plugins: [nodeResolve(), commonjs(), typescript(), paths(), svgr(), remap(), isProd && terser()],
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      typescript(),
+      paths(),
+      svgr(),
+      externals({
+        "@/components/utils": "./utils.js",
+        "@/components/spinner": "./spinner.js",
+        "@/components/popup": "./popup.js",
+        "@/components/text": "./text.js",
+        "@/components/input-helpers": "./input-helpers.js",
+      }),
+      isProd && terser(),
+    ],
     external: [
       "react",
       "react/jsx-runtime",
@@ -70,7 +106,7 @@ const componentConfigs = defineConfig([
     ],
   },
   {
-    input: componentInputMap,
+    input: componentPathMap,
     output: { dir: "dist" },
     plugins: [typescript(), dts()],
   },
