141a9325f49532e46735e287f0270ba953e1a148
Kwame Opare Asiedu <asieduoparekwame@gmail.com>
2024-11-25 18:47:19 +0000

    Added sample app to showcase usage

--- apps/vite/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import polymorphUi from "polymorphui/plugin-vite";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), polymorphUi()],
-});
